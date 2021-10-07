fb1d460f525d33b14ae8a482009e145f9c045cda
nishitjain1204 <yonishitjain123456@gmail.com>
2021-10-07 20:58:37 +0530

    disease detection

--- src/components/pages/DiseaseUpload.js
+++ src/components/pages/DiseaseUpload.js
@@ -1,20 +1,35 @@
 import React from "react";
 import { useState } from "react";
 import Header from "../layout/Header";
+import axios from "axios";
 
 const DiseaseUpload = () => {
-  const handleFile = (e) => {
-    console.log("handle ebent", e.target.files);
-  };
-  //   const [picture, setPicture] = useState({});
+  
   const [selectedImage, setSelectedImage] = useState();
 
   // This function will be triggered when the file field change
   const imageChange = (e) => {
     if (e.target.files && e.target.files.length > 0) {
+      console.log('image change',e.target.files[0])
+
       setSelectedImage(e.target.files[0]);
     }
   };
+
+  const getHealth = (e)=>{
+    let file = selectedImage;
+    let formdata = new FormData();
+    formdata.append('file', file)
+
+    console.log(e.target)
+    console.log(selectedImage)
+    axios({
+      url: 'http://127.0.0.1:8000/disease-predict',
+      method: "POST",
+      data: formdata}).then(response=>{
+    console.log("add this",response );
+    })
+  }
 
   // This function will be triggered when the "Remove This Image" button is clicked
   const removeSelectedImage = () => {
@@ -26,25 +41,17 @@ const DiseaseUpload = () => {
       {/* <Header /> */}
       <h3 style={styles.textCenter}>🌾Disease Detection🌾</h3>
       <div className="m-3">
-        <input
-          onChange={(e) => {
-            this.handleFile(e);
-          }}
-          className="d-none"
-          accept="image/*"
-          type="file"
-          onChange={imageChange}
-        />
+      <input type="file" name="file" id="image" onChange={imageChange}/>
 
         {selectedImage && (
           <div style={styles.preview}>
-            <img
+            <img id="image_api"
               src={URL.createObjectURL(selectedImage)}
               style={styles.image}
               alt="Thumb"
             />
             <br />
-            <button style={styles.upload}>Analyse the disease</button>
+            <button onClick={getHealth} style={styles.upload}>Analyse the disease</button>
             <br />
             <button onClick={removeSelectedImage} style={styles.delete}>
               Remove This Image
